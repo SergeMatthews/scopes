55df18d5f9d22feac80cb583b6ee59c771e8fb0e
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-22 09:34:41 +0600

    Add files via upload

---
+++ scope1_count.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void counting_function();
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "RU");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+
+    return 0;
+}
+
+void counting_function() {
+    static unsigned int count = 0;
+    std::cout << "Количество вызовов функции counting_function(): " << ++count << std::endl;
+}
+
